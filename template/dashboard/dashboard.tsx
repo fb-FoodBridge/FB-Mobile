0d6368351accda282e6c3f668c62c4e7c6035cb7
ooliveiratg <menezesth16@gmail.com>
2026-02-02 17:02:27 -0300

    feat(dashboard): retrieving data from the database to create the graph

--- template/dashboard/dashboard.tsx
+++ template/dashboard/dashboard.tsx
@@ -2,16 +2,73 @@ import { View, Text } from "react-native";
 import Logo from "assets/svg/Logo.svg";
 import PieChartSvg from "assets/svg/icons/pieChart.svg";
 import { PieChart } from "react-native-gifted-charts"
+import React,{ useEffect, useState } from "react";
+import { handleCallApi } from "services/handleCallApi";
+import { ListingDonation } from "services/donation/listing";
+import { decodeToken, DonationList } from "interface/interfaces";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { jwtDecode } from "jwt-decode";
+import { set } from "node_modules/zod/index.cjs";
+
 
 export function DashboardTemplate() {
+    const [donations, setDonations] = useState<DonationList[] | []>([])
+    const [decodedToken, setDecodedToken] = useState<decodeToken | null>(null)
+    const [peddingDonations , setPeddingDonations] = useState<number>(0)
+    const [rejectedDonations , setRejectedDonations] = useState<number>(0)
+    const [acceptedDonations , setAcceptedDonations] = useState<number>(0)
     const data = () => {
         const pieData = [
-            { value: 40, focused:true, color: '#FFB74D' }, // Aceitas
-            { value: 30, color: '#6CBE77' }, // Recusadas
-            { value: 30, color: '#0077B6' }, // Pendente
+            { value: acceptedDonations, focused:maxValue === acceptedDonations, color: '#FFB74D' },
+            { value: rejectedDonations, focused:maxValue === rejectedDonations, color: '#6CBE77' },
+            { value: peddingDonations, focused:maxValue === peddingDonations, color: '#0077B6' }, 
         ];
         return pieData;
     }
+
+
+    const maxValue = Math.max(acceptedDonations, rejectedDonations, peddingDonations);
+ 
+    useEffect(() => {
+        async function getTokenDecoded(){
+            const token = await AsyncStorage.getItem('token')
+            if(!token) return null
+            const decoded = await jwtDecode<decodeToken>(token)
+                setDecodedToken(decoded)
+                console.log(decoded)
+        }
+       
+        async function handleListDonations(){
+            const response = await handleCallApi(ListingDonation, {})
+            if(!response.data) return null
+                setDonations(response.data)
+                console.log(response)
+        }
+        getTokenDecoded()
+        handleListDonations()
+    },[])
+    useEffect(() => {
+                const accpetedDonations = donations.filter(
+            donation => donation.merchant_id === decodedToken?.id && donation.status === "completed"
+        ).length
+        setAcceptedDonations(accpetedDonations)
+        const pendingDonations = donations.filter(
+            donation => donation.merchant_id === decodedToken?.id && donation.status === "pending"
+        ).length
+        setPeddingDonations(pendingDonations)
+        const rejectedDonations = donations.filter(
+            donation => donation.merchant_id === decodedToken?.id && donation.status === "cancelled"
+        ).length
+        setRejectedDonations(rejectedDonations)
+
+        const teste = donations.some(
+            donation => donation.merchant_id=== decodedToken?.id
+        )
+        console.log("testando ai ACEITO:: " + accpetedDonations)
+        console.log("testando ai PENDENTE:: " + pendingDonations)
+        console.log("testando ai RECUSADO:: " + rejectedDonations)
+        console.log("testando ai TOTAL:: " + teste)
+    },[donations])
   return (
     <View className="flex-1 bg-black800">
       <View className="w-full ml-[25px] flex-col gap-[30px] mt-10">
@@ -58,7 +115,7 @@ export function DashboardTemplate() {
             <PieChart
             data={data()}
             innerRadius={60}
-            radius={90}
+            radius={100}
             sectionAutoFocus
             showValuesAsLabels
             textColor="#FFFFFF"
